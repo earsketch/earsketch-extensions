5bf0080eb81c91a5229e877c6bc603fb59443af3
Luke Heerman <43624284+heerman@users.noreply.github.com>
2026-08-04 23:18:39 -0400

    Centralize linting (#16)
    Configured eslint and prettier to use a shared setup across the
    repository

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,16 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import prettierRecommended from 'eslint-plugin-prettier/recommended'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['**/dist']),
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    files: ['extensions/**/*.{ts,tsx}', 'sdk/**/*.ts'],
+    extends: [js.configs.recommended, tseslint.configs.recommended],
     languageOptions: {
       globals: globals.browser,
       parserOptions: {
@@ -22,4 +18,12 @@ export default defineConfig([
       },
     },
   },
+  {
+    files: ['extensions/*/src/**/*.{ts,tsx}'],
+    extends: [
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+  },
+  prettierRecommended,
 ])
