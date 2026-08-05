38931b9a50d111fede412ba43f4f4c3c21cc3243
Luke Heerman <43624284+heerman@users.noreply.github.com>
2026-08-04 22:53:24 -0400

    Format repository with prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,15 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import prettierRecommended from 'eslint-plugin-prettier/recommended'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js"
+import globals from "globals"
+import prettierRecommended from "eslint-plugin-prettier/recommended"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import tseslint from "typescript-eslint"
+import { defineConfig, globalIgnores } from "eslint/config"
 
 export default defineConfig([
-  globalIgnores(['**/dist']),
+  globalIgnores(["**/dist"]),
   {
-    files: ['extensions/**/*.{ts,tsx}', 'sdk/**/*.ts'],
+    files: ["extensions/**/*.{ts,tsx}", "sdk/**/*.ts"],
     extends: [js.configs.recommended, tseslint.configs.recommended],
     languageOptions: {
       globals: globals.browser,
@@ -19,11 +19,8 @@ export default defineConfig([
     },
   },
   {
-    files: ['extensions/*/src/**/*.{ts,tsx}'],
-    extends: [
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    files: ["extensions/*/src/**/*.{ts,tsx}"],
+    extends: [reactHooks.configs.flat.recommended, reactRefresh.configs.vite],
   },
   prettierRecommended,
 ])
