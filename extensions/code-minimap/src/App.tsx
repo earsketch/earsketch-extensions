1e65ed2cd8f766e29831b97b623bfc0d62035883
Luke Heerman <43624284+heerman@users.noreply.github.com>
2026-09-16 14:49:57 -0400

    Use shared sdk functions

--- extensions/code-minimap/src/App.tsx
+++ extensions/code-minimap/src/App.tsx
@@ -1,49 +1,15 @@
+import { requestEarSketch } from "@earsketch/extension-sdk"
 import { useEffect, useState } from "react"
 import "./App.css"
 
-function requestEarSketch<T>(fn: string): Promise<T> {
-  return new Promise((resolve, reject) => {
-    const handleMessage = (event: MessageEvent) => {
-      if (
-        event.source !== window.parent
-        // || event.origin !== "http://localhost:5173" // TODO whats wrong here
-      ) {
-        return
-      }
-
-      window.removeEventListener("message", handleMessage)
-
-      try {
-        const result =
-          typeof event.data === "string" ? JSON.parse(event.data) : event.data
-
-        if (result?.error) {
-          reject(new Error(result.error))
-          return
-        }
-
-        resolve(result as T)
-      } catch {
-        // getEditorContents returns the script directly, which might not be JSON.
-        resolve(event.data as T)
-      }
-    }
-
-    window.addEventListener("message", handleMessage)
-
-    window.parent.postMessage(JSON.stringify({ fn }), "*")
-  })
-}
-
 function App() {
   const [code, setCode] = useState("")
-  async function fetchCode() {
-    const contents = await requestEarSketch<string>("getEditorContents")
-
-    setCode(contents)
-  }
-
   useEffect(() => {
+    async function fetchCode() {
+      const contents = await requestEarSketch<string>("getEditorContents")
+      setCode(contents)
+    }
+
     fetchCode()
 
     const interval = setInterval(() => {
